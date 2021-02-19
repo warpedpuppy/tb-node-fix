676e4a374a0505a52910a3d6dd2338b3c412a924
Ty4jbt <tyaboe@gmail.com>
2021-02-19 11:13:03 -0800

    added gitignore

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express = require('express'),
-    app = express();
+    morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my app!');
+});
+
+app.get('/secreturl', (req, res) => {
+    res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
 
