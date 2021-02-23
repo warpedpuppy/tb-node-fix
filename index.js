6ec26e9080ddbb9adb7c2493fa5a3ea0e558765a
Ty4jbt <tyaboe@gmail.com>
2021-02-22 23:29:13 -0800

    added express functionality

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express'),
 
 const app = express();
 
+app.use(morgan('common'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something is broken');
+});
+
 let topMovies = [
     {
         title: 'The Other Guys',
@@ -115,9 +122,27 @@ app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
 
-app.use(morgan('common'));
 
+
+// Listens for requests
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
+});
+
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
