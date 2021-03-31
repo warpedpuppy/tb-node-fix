e78de6a23ee1813acddef928ac0a0543f67cc6cd
Ty4jbt <70467306+Ty4jbt@users.noreply.github.com>
2021-03-30 17:15:48 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET || 'My_secret',
-    DB_CONNECT: process.env.DB_CONNECT || 'mongodb://localhost:27017/myFlixDB',
-    PORT: process.env.PORT || 8080
-}
